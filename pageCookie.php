7eca19c2c91c8b7250c1a423f5508dcc3d90b987
ABoileauGIT <2393443@csfoy.ca>
2023-06-25 08:04:35 -0400

    tentative strict_type=1

--- pageCookie.php
+++ pageCookie.php
@@ -1,3 +1,6 @@
+<?php //declare (strict_types=1);?> <!-- Je recois plusieurs erreurs php dans mon browser lorsque 
+j'essaie d'inclure strict_types=1 . J'ai corrigé à plusieurs reprises, mais je recois une nouvelle
+erreur chaque fois que j'en corrige une en spécifiant le type, function, ect. -->
 <!DOCTYPE html>
 <html lang="fr">
 
@@ -10,16 +13,15 @@
 
 <body>
     <?php
-    $nomCookie = 'cookieVisite';
     $nbVisites = 0;
     $expiration = Time() + 5;  // 5 secondes
 
-    if (!isset($_COOKIE[$nomCookie])) {
+    if (!isset($_COOKIE['compteurVisites'])) {
         $nbVisites = 1;
     } else {
-        $nbVisites = $_COOKIE[$nomCookie] + 1;
+        $nbVisites = $_COOKIE['compteurVisites'] + 1;
     }
-    setcookie($nomCookie, $nbVisites, $expiration);
+     setcookie('compteurVisites', $nbVisites, $expiration);
     echo '<h1>La page a été visité ' . $nbVisites . ' fois.</h1>';
     ?>
 </body>
